15bde5064a6ef59e53bf43e8130765b4fe9e9693
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2025-08-29 23:48:57 +0530

    py file add

---
+++ 7 Modules and packages/main.py
@@ -0,0 +1,9 @@
+from mypackage import module1,module2
+
+result_ad = module1.add(5,4)
+result_mul = module2.multiply(5,4)
+
+print(f'Addition: {result_ad}')
+print(f'Multiplication: {result_mul}')
+
+#your code and folder structue working fine...
